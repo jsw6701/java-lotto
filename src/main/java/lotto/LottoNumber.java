1c829eec2d80c42fd0c2ad7cd7431b52760746e5
jsw6701 <anes53027@gmail.com>
2022-11-14 11:07:35 +0900

    feature(LottoNumber) : 상수화 및 생성자 구현

---
+++ src/main/java/lotto/LottoNumber.java
@@ -0,0 +1,15 @@
+package lotto;
+
+public class LottoNumber {
+    static final int MIN_RANGE = 1;
+    static final int MAX_RANGE = 45;
+
+    private final int number;
+
+
+    public LottoNumber(int number) {
+        this.number = number;
+    }
+}
+
+
